5cf18dc8d698f5f1223d34c54178aec8f3eb5d36
deepaksonawane116 <72155548+deepaksonawane116@users.noreply.github.com>
2020-10-01 23:37:19 +0530

    Update Solution.java
    to find uppercase or lowecase character

--- Solution.java
+++ Solution.java
@@ -1,1 +1,22 @@
-String pattern = "^[aA-zZ]\\w{7,29}$";
+package thirteenthquestion;
+import java.util.Scanner;
+
+
+public class TypeOfCharacter {
+public static void main(String[] args){
+
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter character");
+	//int a= sc.nextInt();
+	char a= sc.next().charAt(0);
+	
+	if(a<=57&&a>=48)
+	{
+		System.out.println("Character is number");
+	}else if(a<=90&&a>=65||a<=122&&a>=97)
+		{
+			System.out.println("Character is alphabet");
+		}
+		else
+			System.out.println("Character is Special type");
+	
